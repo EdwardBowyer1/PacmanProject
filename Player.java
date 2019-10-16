0e553adf3e7bcba05c26ec6b2d6af293145e3bec
EdwardBowyer1 <56053624+EdwardBowyer1@users.noreply.github.com>
2019-10-15 18:25:11 -0600

    Update to Player

--- Player.java
+++ Player.java
@@ -1,10 +1,23 @@
+//Imports
 import java.util.Scanner;
 
 public class Player
 {
     //Class to set up the player, and how the player input works and how the player interacts with the AI
+    //Instance Variables
     private Location location;
     
+    public Player()
+    {
+        //Empty Constructor
+    }
+    
+    public Player(Location Location)
+    {
+        Player.location = Location;
+    }
+
+    //Method to control movement
     public void Move(char input)
     {
         if (input == 'a')
@@ -16,15 +29,10 @@ public class Player
         else if (input == 'd')
         Player.xlocation += 1;
     }
-    public void Die() //Name for this method?
+
+    //Method for interaction with Ghosts
+    public void Die() 
     {
         //Gives response to player coming into contact with AI (when powerup not active)
     }
-
-    /*
-    public main()
-    {
-        //Tests class
-    }
-    */
 }
