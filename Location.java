549fbb12371f6cba10f77d0493041d0995e7a8ce
MedhanieG <medhanieug@gmail.com>
2019-10-15 18:27:16 -0600

    Add files via upload

--- Location.java
+++ Location.java
@@ -1,9 +1,33 @@
 
 public class Location {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	//instance variables
+	private int xlocation;
+	private int ylocation;
+	
+	//constructor
+	public Location(int xlocation, int ylocation) 
+	{
+		this.xlocation = xlocation;
+		this.ylocation = ylocation;
+	}
+	//getters and setters
+	public int getXlocation() {
+		return xlocation;
 	}
 
+	public void setXlocation(int xlocation) {
+		this.xlocation = xlocation;
+	}
+
+	public int getYlocation() {
+		return ylocation;
+	}
+
+	public void setYlocation(int ylocation) {
+		this.ylocation = ylocation;
+	}
+	
+	
+	
 }
+
