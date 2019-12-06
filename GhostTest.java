e33bc62b44f54f4a9111ad31e6ef060c3d078fa6
MedhanieG <medhanieug@gmail.com>
2019-12-06 02:49:52 -0700

    Update GhostTest.java

--- GhostTest.java
+++ GhostTest.java
@@ -1,10 +1,8 @@
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
-
 import javafx.embed.swing.JFXPanel;
 
 /**
